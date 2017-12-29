a0a89beecab71a2f23e8de2ab2287010da8597d5
koppepan <lalalakoppepan86@gmail.com>
2017-12-29 19:35:37 +0900

    体裁整え

--- main.js
+++ main.js
@@ -1,72 +1,88 @@
 "use strict";
 
-const SCREEN_WIDTH = 400;
-const SCREEN_HEIGHT = 400;
-const NUM_OF_CELL = 50;
+//定数
+const SCREEN_WIDTH = 200;
+const SCREEN_HEIGHT = 200;
+const NUM_OF_CELL = 20;
 const CELL_SIZE = SCREEN_WIDTH / NUM_OF_CELL;
 const FPS = 10;
+
+//
 let canvas;
 let context;
 
-window.onload = function(){
-    let field = new Array(SCREEN_WIDTH);
-    let tempField = new Array(SCREEN_WIDTH);
+//load
+window.onload = function(){]
+    //init field
+    let field = new Array(NUM_OF_CELL);
+    let tempField = new Array(NUM_OF_CELL);
 
-    for(let i = 0; i < SCREEN_WIDTH; i++){
-        field[i] = new Array(SCREEN_HEIGHT);
+    for(let i = 0; i <NUM_OF_CELL; i++){
+        field[i] = new Array(NUM_OF_CELL);
+        tempField[i] = new Array(NUM_OF_CELL);
     }
+    
 
-    for(let i = 0; i < SCREEN_WIDTH; i++){
-        for(let j = 0; j < SCREEN_HEIGHT; j++){
+    for(let i = 0; i < NUM_OF_CELL; i++){
+        for(let j = 0; j < NUM_OF_CELL; j++){
             field[i][j] = Math.floor(Math.random()*2);
+
+            //テスト用
+            //field[i][j] = j % 2;
         }
     }
 
+    //init canvas
     canvas = document.getElementById("canvas");
     canvas.width = SCREEN_WIDTH;
     canvas.height = SCREEN_HEIGHT;
     context = canvas.getContext("2d");
     context.fillStyle = "rgb(100, 100, 0)";
+ 
     update(field, tempField);
+    
 }
 
+//更新部分
 function update(field, tempField){
+    calc(field, tempField);
+    draw(field);
+    setTimeout(update, 1000/FPS, fieldn, tempField);
+}
+
+//ロジック部分
+function calc(field, tempField){
     let existLife = 0;
     tempField = field.slice();
-    
-    for(let i = 0; i < SCREEN_WIDTH; i++){
-        for(let j = 0; j < SCREEN_HEIGHT; j++){
+    console.log(tempField == field);
+    for(let i = 0; i < NUM_OF_CELL; i++){
+        for(let j = 0; j < NUM_OF_CELL; j++){
             existLife = 0;
             for(let x = -1; x < 2; x++){
                 for(let y = -1; y < 2; y++){
                     if(x == 0 && y == 0) continue;
-                    if(i + x < SCREEN_WIDTH && j + y < SCREEN_HEIGHT && 
-                       i + x >= 0           && j + y >= 0){
-                        if(tempField[i+x][j+y] == 1){
-                            existLife++;
-                        }
+                    if(0 <= i + x && i + x < NUM_OF_CELL && 0 <= j + y && j + y < NUM_OF_CELL){
+                        if(tempField[i+x][j+y] == 1)existLife++;
                     }
                 }
             }
 
-            if(tempField[i][j] && (existLife == 2 || existLife == 3)){
+            if(tempField[i][j] == 1 && (existLife == 2 || existLife == 3)){
                 field[i][j] = 1;
-            }else if(!tempField[i][j] && existLife == 3){
+            }else if(tempField[i][j] == 0 && existLife == 3){
                 field[i][j] = 1;
             }else{
                 field[i][j] = 0;
             }
         } 
     }
-    draw(field);
-    setTimeout(update, 1000/FPS, field, tempField);
-    
 }
 
+//描画部分
 function draw(field){
     context.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    for(let i = 0; i < SCREEN_WIDTH; i++){
-        for(let j = 0; j < SCREEN_HEIGHT; j++){
+    for(let i = 0; i < NUM_OF_CELL; i++){
+        for(let j = 0; j < NUM_OF_CELL; j++){
             if(field[i][j]) context.fillRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
         }
     }
